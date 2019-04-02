609ad7d8451de374c4b4dcc53223fb08e8f220de
lifeng93 <1079757337@qq.com>
2019-04-02 08:45:58 +0800

    decorator referer with port

--- users/decorators.py
+++ users/decorators.py
@@ -5,7 +5,7 @@ def referer_required(view_func):
     def _wrapped_view(request, *args, **kwargs):
         referer = request.META.get('HTTP_REFERER')
         if referer:
-            re_pattern = '^(?=^.{3,255}$)(http(s)?:\/\/)?(www\.)?[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+'
+            re_pattern = '^(?=^.{3,255}$)(http(s)?:\/\/)?(www\.)?[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+(:[0-9]{1,5})?'
             dns = re.match(re_pattern, referer).group()
             allow_host = ['http://'+request.META['HTTP_HOST']]
             if dns in allow_host:
